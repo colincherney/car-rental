54cf53179b3eb5501118c98415cd3eadc560c3de
colin11702 <colinmcherney@gmail.com>
2023-08-01 10:14:24 -0700

    Added Rental Car Section

---
+++ script.js
@@ -0,0 +1,23 @@
+function showBentley() {
+  $("#car-image").attr("src", "bentley.jpg");
+}
+
+function showCadillac() {
+  $("#car-image").attr("src", "cadillac.jpg");
+}
+
+function showJeep() {
+  $("#car-image").attr("src", "jeep.jpg");
+}
+
+function showPorsche() {
+  $("#car-image").attr("src", "porsche.jpg");
+}
+
+function showLincoln() {
+  $("#car-image").attr("src", "lincoln.jpg");
+}
+
+function showChevy() {
+  $("#car-image").attr("src", "chevy.jpg");
+}
